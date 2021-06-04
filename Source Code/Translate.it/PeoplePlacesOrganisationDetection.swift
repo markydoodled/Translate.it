c6a7ad0150bb3f307d34eef146b2587256e88deb
Mark Howard <markhoward2005@gmail.com>
2021-06-04 10:05:40 +0100

    Updated Source Code

---
+++ Source Code/Translate.it/PeoplePlacesOrganisationDetection.swift
@@ -0,0 +1,20 @@
+//
+//  PeoplePlacesOrganisationDetection.swift
+//  Translate.it
+//
+//  Created by Mark Howard on 31/05/2021.
+//
+
+import SwiftUI
+
+struct PeoplePlacesOrganisationDetection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PeoplePlacesOrganisationDetection_Previews: PreviewProvider {
+    static var previews: some View {
+        PeoplePlacesOrganisationDetection()
+    }
+}
---
+++ Source Code/Translate.it/TextSpeechClassification.swift
@@ -0,0 +1,20 @@
+//
+//  TextSpeechClassification.swift
+//  Translate.it
+//
+//  Created by Mark Howard on 02/06/2021.
+//
+
+import SwiftUI
+
+struct TextSpeechClassification: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextSpeechClassification_Previews: PreviewProvider {
+    static var previews: some View {
+        TextSpeechClassification()
+    }
+}
